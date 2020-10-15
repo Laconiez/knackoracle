a450ca66a5d78747a07be1e1b8f37ac5f1c9b9eb
Pavel Klimenko <pavel.klimenko@distillery.com>
2020-10-15 22:04:04 +0300

    check firebase

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { db } from 'utils/firebase';
 
 const Main = () => {
+  useEffect(() => {
+    db.collection('testit')
+      .get()
+      .then((d: any) => {
+        const t: any = [];
+        d.forEach((doc: any) => t.push(doc.data()));
+
+        console.log(t);
+      });
+  }, []);
+
   return <div>KnackOracle</div>;
 };
 
